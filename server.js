ef11dfe2b400e96d5324d0cf40376b82d70f4c83
Harshinboy <153446599+Harshinboy@users.noreply.github.com>
2024-09-03 23:19:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,13 +4,11 @@ const morgan = require("morgan");
 const colors = require("colors");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
 
 //env config
 dotenv.config();
-
-//router import
-const userRoutes = require("./routes/userRoutes");
-const blogRoutes = require("./routes/blogRoutes");
 
 //mongodb connection
 connectDB();
@@ -23,12 +21,33 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
+// Session middleware configuration
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || "your_default_secret",
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGO_URL, // MongoDB connection URI
+      ttl: 14 * 24 * 60 * 60, // Session expiration time (14 days)
+    }),
+    cookie: {
+      maxAge: 14 * 24 * 60 * 60 * 1000, // Cookie expiration time (14 days)
+    },
+  })
+);
+
+//router import
+const userRoutes = require("./routes/userRoutes");
+const blogRoutes = require("./routes/blogRoutes");
+
 //routes
 app.use("/api/v1/user", userRoutes);
 app.use("/api/v1/blog", blogRoutes);
 
 // Port
 const PORT = process.env.PORT || 8080;
+
 //listen
 app.listen(PORT, () => {
   console.log(
