f3e1e455910288a5e4596e09084b96fcb5d7a7b6
Harshinboy <153446599+Harshinboy@users.noreply.github.com>
2024-09-03 23:20:03 +0530

    Update userContoller.js

--- controllers/userContoller.js
+++ controllers/userContoller.js
@@ -1,5 +1,6 @@
 const userModel = require("../models/userModel");
 const bcrypt = require("bcrypt");
+
 //create user register user
 exports.registerController = async (req, res) => {
   try {
@@ -8,15 +9,15 @@ exports.registerController = async (req, res) => {
     if (!username || !email || !password) {
       return res.status(400).send({
         success: false,
-        message: "Please Fill all fields",
+        message: "Please fill all fields",
       });
     }
-    //exisiting user
-    const exisitingUser = await userModel.findOne({ email });
-    if (exisitingUser) {
+    //existing user
+    const existingUser = await userModel.findOne({ email });
+    if (existingUser) {
       return res.status(401).send({
         success: false,
-        message: "user already exisits",
+        message: "User already exists",
       });
     }
     const hashedPassword = await bcrypt.hash(password, 10);
@@ -24,6 +25,10 @@ exports.registerController = async (req, res) => {
     //save new user
     const user = new userModel({ username, email, password: hashedPassword });
     await user.save();
+
+    // Set user session
+    req.session.userId = user._id;
+
     return res.status(201).send({
       success: true,
       message: "New User Created",
@@ -32,7 +37,7 @@ exports.registerController = async (req, res) => {
   } catch (error) {
     console.log(error);
     return res.status(500).send({
-      message: "Error In Register callback",
+      message: "Error in Register callback",
       success: false,
       error,
     });
@@ -46,14 +51,14 @@ exports.getAllUsers = async (req, res) => {
     return res.status(200).send({
       userCount: users.length,
       success: true,
-      message: "all users data",
+      message: "All users data",
       users,
     });
   } catch (error) {
     console.log(error);
     return res.status(500).send({
       success: false,
-      message: "Error In Get ALl Users",
+      message: "Error in Get All Users",
       error,
     });
   }
@@ -67,34 +72,65 @@ exports.loginController = async (req, res) => {
     if (!email || !password) {
       return res.status(401).send({
         success: false,
-        message: "Please provide email or password",
+        message: "Please provide email and password",
       });
     }
     const user = await userModel.findOne({ email });
     if (!user) {
       return res.status(200).send({
         success: false,
-        message: "email is not registerd",
+        message: "Email is not registered",
       });
     }
-    //password
+    //password check
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
       return res.status(401).send({
         success: false,
-        message: "Invlid username or password",
+        message: "Invalid email or password",
       });
     }
+
+    // Set user session
+    req.session.userId = user._id;
+
     return res.status(200).send({
       success: true,
-      messgae: "login successfully",
+      message: "Login successful",
       user,
     });
   } catch (error) {
     console.log(error);
     return res.status(500).send({
       success: false,
-      message: "Error In Login Callcback",
+      message: "Error in Login callback",
+      error,
+    });
+  }
+};
+
+//logout
+exports.logoutController = async (req, res) => {
+  try {
+    req.session.destroy((err) => {
+      if (err) {
+        return res.status(500).send({
+          success: false,
+          message: "Error in Logout callback",
+          error: err,
+        });
+      }
+      res.clearCookie("connect.sid"); // clear the session cookie
+      return res.status(200).send({
+        success: true,
+        message: "Logout successful",
+      });
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({
+      success: false,
+      message: "Error in Logout callback",
       error,
     });
   }
